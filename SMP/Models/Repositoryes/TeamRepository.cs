a3342871492160a1582451731c3298a2632b8e2f
Илья <bkmz.59@gmail.com>
2018-04-17 11:45:37 +0500

    Добавлены коментарии к классу репозиторию функций класса Team

--- SMP/Models/Repositoryes/TeamRepository.cs
+++ SMP/Models/Repositoryes/TeamRepository.cs
@@ -14,26 +14,52 @@ namespace SMP.Models.Repositoryes
             cont = _cont;
         }
 
+        /// <summary>
+        /// Возвращает все команды из базы
+        /// </summary>
+        /// <returns>Список команд</returns>
         public List<Team> GetTeams()
         {
             return cont.Team.OrderBy(p => p.IdTeam).ToList();
         }
 
+        /// <summary>
+        /// Возвращает команду по Id исполнителя и проекта
+        /// </summary>
+        /// <param name="personId">Id исполнителя</param>
+        /// <param name="projectId">Id проекта</param>
+        /// <returns>Команду</returns>
         public Team GetTeam(int personId, int projectId)
         {
             return cont.Team.ToList().Find(t => t.Person.IdPerson == personId && t.Project.IdProject == projectId);
         }
 
+        /// <summary>
+        /// Возвращает команды исполнителя
+        /// </summary>
+        /// <param name="personId">Id исполнителя</param>
+        /// <returns>Список команд</returns>
         public List<Team> GetTeamsByPerson(int personId)
         {
             return cont.Team.ToList().FindAll(t => t.Person.IdPerson == personId);
         }
 
+        /// <summary>
+        /// Возвращает команду проекта/работы
+        /// </summary>
+        /// <param name="projectId">Id проекта</param>
+        /// <returns>Список команд</returns>
         public List<Team> GetTeamsByProject(int projectId)
         {
             return cont.Team.ToList().FindAll(t => t.Project.IdProject == projectId);
         }
 
+        /// <summary>
+        /// Добавляет команду в базу
+        /// </summary>
+        /// <param name="personId">Id исполнителя</param>
+        /// <param name="projectId">Id проекта</param>
+        /// <returns>Добавленную команду</returns>
         public Team AddTeam(int personId, int projectId)
         {
             Team t = new Team
