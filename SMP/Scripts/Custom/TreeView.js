487792cfc5cda4931e60eba0b4b28e98df96dcb7
Andrey <andriocactus@gmail.com>
2018-10-20 10:53:25 +0500

    Добавил tree view для работ

---
+++ SMP/Scripts/Custom/TreeView.js
@@ -0,0 +1,33 @@
+﻿/*
+ * Срабатывает при клике на экспандер дерева.
+ */
+function OnExpanderClick() {
+    $('.expander').click(function() {
+        ShowHide($(event.target).closest('.treeNode'));
+    });
+}
+
+/*
+ * Сворачивает или разворачивает дерево в узле.
+ */
+function ShowHide($node) {
+    var $node = $($node).closest('.treeNode');
+    var $children = $(".treeNode[pId = '" + $node.attr('id') + "']");
+    if ($node.hasClass('expanded')) {
+        $node.removeClass('expanded');
+        $children.addClass('hidden');
+    } else {
+        $node.addClass('expanded');
+        $children.removeClass('hidden');
+    }
+
+    $children.each(function(ind, $el) {
+        ShowHide($el);
+    });
+}
+
+function OnDocLoad() {
+    $(document).ready(OnExpanderClick);
+}
+
+OnDocLoad();
