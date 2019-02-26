ac521caa56260e830e656bda91aed515cf82a09d
Andrey <andriocactus@gmail.com>
2019-02-27 03:38:39 +0500

    Исправил формирвоание отчета. Исправил дополниьельные критические ошибки.

--- SMP/ViewModels/ManagerReportViewModel.cs
+++ SMP/ViewModels/ManagerReportViewModel.cs
@@ -38,11 +38,123 @@ namespace SMP.ViewModels
 
             WorksCompleted = allLowestChildren.Count(proj => proj.isClose);
             WorksInTotal = allLowestChildren.Count;
-            CompletionRate = (double) WorksCompleted / WorksInTotal;
+            CompletionRate = (double)WorksCompleted / WorksInTotal;
 
-            var daysUsed = TopLevelCompletedWorks.Sum(work => (work.startDateTime - work.endDateTime).Days);
-            ReserveDaysUsed = daysUsed - (project.endDateTime - project.startDateTime).Days - project.reserve;
+            var reserveStart = project.endDateTime - TimeSpan.FromDays(project.reserve);
+
+            var intervals = new IntervalsSet();
+            foreach (var topChild in allTopChildren)
+            {
+                if (topChild.endDateTime >= reserveStart)
+                {
+                    intervals.Add(new Interval
+                    {
+                        Strat = topChild.startDateTime >= reserveStart
+                                        ? topChild.startDateTime
+                                        : reserveStart,
+                        End = topChild.endDateTime
+                    }); 
+                }
+            }
+
+            ReserveDaysUsed = intervals.Intervals.Sum(inter => (inter.End - inter.Strat).Value.Days + 1);
             ReserveDaysRate = (double)ReserveDaysUsed / project.reserve;
+        }
+
+        // Объявляю классы для работы с интервалами тут, чтобы было быстрее.
+
+        /// <summary>
+        /// Набор интервалов.
+        /// </summary>
+        private class IntervalsSet
+        {
+            internal List<Interval> Intervals;
+
+            public IntervalsSet()
+            {
+                Intervals = new List<Interval>();
+            }
+
+            /// <summary>
+            /// Добавить интервал в набор.
+            /// </summary>
+            public void Add(Interval interval)
+            {
+                Intervals.Add(interval);
+
+                // Нормализовать полученный набор с учетом нового интервала.
+                while (!NormalizeAddition()) { }
+            } 
+
+            /// <summary>
+            /// Нормализовать набор интервалов при добавлении - склеить пересекающеся.
+            /// При вызове набор должен быть уже нормальным.
+            /// </summary>
+            /// <returns></returns>
+            private bool NormalizeAddition()
+            {
+                // Todo предусмотреть лучи - интервалы с одним концом.
+                var toRemove = new List<Interval>();
+                Interval newInterval = null;
+                foreach (var lvlOne in Intervals)
+                {
+                    if (toRemove.Any())
+                    {
+                        break;
+                    }
+
+                    foreach (var lvlTwo in Intervals)
+                    {
+                        if (lvlOne != lvlTwo)
+                        {
+                            var primal = lvlOne;
+                            var secondary = lvlTwo;
+                            if (primal.Strat > secondary.Strat)
+                            {
+                                primal = lvlTwo;
+                                secondary = lvlOne;
+                            }
+
+                            // Если пересекаются, то сплющиваем.
+                            if (secondary.Strat <= primal.End)
+                            {
+                                newInterval = new Interval
+                                {
+                                    Strat = primal.Strat,
+                                    End = primal.End > secondary.End
+                                          ? primal.End
+                                          : secondary.End
+                                };
+                                toRemove = new List<Interval> { primal, secondary };
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (toRemove.Any())
+                {
+                    foreach (var inter in toRemove)
+                    {
+                        Intervals.Remove(inter);
+                    }
+
+                    Intervals.Insert(0, newInterval);
+
+                    return false;
+                }
+
+                return true;
+            }
+
+            // Todo добавить метод для нормализации набора интервалов при вычитании.
+        }
+
+        private class Interval
+        {
+            public DateTime? Strat;
+
+            public DateTime? End;
         }
     }
 }
